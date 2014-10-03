ff45371b1804414643974e8b4a0e9d0fa796eaef
eNorris <812xsi@gmail.com>
2014-10-02 21:11:48 -0500

    clone() and rotate_about_2d() are now required for CombinatorialBody2d

--- pycharm_project/CombinatorialBody.py
+++ pycharm_project/CombinatorialBody.py
@@ -16,11 +16,18 @@ class CombinatorialBody(object):
     def __contains__(self, item):
         raise Exception("Operator __contains__ not overloaded for this body (" + str(self.__class__.__name__) + ")")
 
+    def clone(self):
+        raise Exception("clone() not overloaded for this body (" + str(self.__class__.__name__) + ")")
+
 
 class CombinatorialBody2d(CombinatorialBody):
 
     def __init__(self):
         super(CombinatorialBody2d, self).__init__()
 
-    def get_rotated_2d(self, theta, about_point, is_radians):
-        raise Exception("get_rotated_2d() not overloaded for this body (" + str(self.__class__.__name__) + ")")
+    def get_rotated_about_2d(self, theta, about_point, is_radians):
+        raise Exception("get_rotated_about_2d() not overloaded for this body (" + str(self.__class__.__name__) + ")")
+
+    def rotate_about_2d(self, theta, about_point, is_radians):
+        raise Exception("rotate_about_2d() not overloaded for this body (" + str(self.__class__.__name__) + ")")
+
