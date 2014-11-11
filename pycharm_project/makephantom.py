aeb9121d6a3d98d8bb14449778659674f1bc5069
eNorris <812xsi@gmail.com>
2014-11-11 11:08:37 -0600

    Increased mesh size to 8x8

--- pycharm_project/makephantom.py
+++ pycharm_project/makephantom.py
@@ -103,7 +103,7 @@ def makeslice():
     airregion.matid = "G"
     airregion.drawevals = True
     airregion.evalpoints.extend([(0, 30, 0.5)])
-    airregions = auxutil.automesh(airregion, (7, 7))
+    airregions = auxutil.automesh(airregion, (8, 8))
 
     #regions.extend([upcolreg, downcolreg, filterreg, bowtieregion])
     #regions.extend([upcolreg, downcolreg, filterreg, bt1region, bt2region, bt3region, bt4region, bt5region, bt6region,
