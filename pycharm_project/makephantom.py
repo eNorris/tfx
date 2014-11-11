9335d137223cbc488eecd56c2e7abc87099dde83
eNorris <812xsi@gmail.com>
2014-11-11 12:11:53 -0600

    Increased mesh to 12x12

--- pycharm_project/makephantom.py
+++ pycharm_project/makephantom.py
@@ -103,7 +103,7 @@ def makeslice():
     airregion.matid = "G"
     airregion.drawevals = True
     airregion.evalpoints.extend([(0, 30, 0.5)])
-    airregions = auxutil.automesh(airregion, (11, 11))
+    airregions = auxutil.automesh(airregion, (12, 12))
 
     #regions.extend([upcolreg, downcolreg, filterreg, bowtieregion])
     #regions.extend([upcolreg, downcolreg, filterreg, bt1region, bt2region, bt3region, bt4region, bt5region, bt6region,
