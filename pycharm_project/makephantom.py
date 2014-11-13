fc077b47b32d209c2a8e5e66f53ce66963a377d7
eNorris <812xsi@gmail.com>
2014-11-13 15:53:31 -0600

    collimator should be rotated correctly now

--- pycharm_project/makephantom.py
+++ pycharm_project/makephantom.py
@@ -215,9 +215,10 @@ def makeslice3d():
     auxutil.rotate_regions([bt1region, bt2region, bt3region, bt4region, bt5region, bt6region, bt7region, bt8region,
                             bt9region, bt10region, bt11region, bt12region, bt13region, btgapregion], 90, is_radians=False)
 
-    for r in [outcolreg, incolreg, filterreg]:
-        # This is okay since none of these regions mutually share bodies
-        r.rotate_about_2d(90, is_radians=False)
+    auxutil.rotate_regions([outcolreg, incolreg, filterreg], 90, is_radians=False)
+    #for r in [outcolreg, incolreg, filterreg]:
+    #    # This is okay since none of these regions mutually share bodies
+    #    r.rotate_about_2d(90, is_radians=False)
 
     air = auxutil.sliceregion(74, 360/16, is_radians=False)
     airbound = rcc2d.Rcc2d(74)
