db9df07c365e01f60a1da4610550659ab0bf614b
eNorris <812xsi@gmail.com>
2014-12-17 17:06:55 -0600

    Added a new file that uses matplotlib to display the flux data graphically

---
+++ pycharm_project/postproc/fluxplotter.py
@@ -0,0 +1,107 @@
+__author__ = 'Edward'
+
+import sys
+import matplotlib.pyplot as pyplot
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri
+import matplotlib.mlab
+import numpy
+
+class FluxPlotter(object):
+
+    def __init__(self, mshfile=None, binary=False):
+        self.mshfile = None
+        self.binary = binary
+        self.data = []
+
+        if mshfile is None:
+            return
+
+        if binary:
+            self.mshfile = open(mshfile, 'rb')
+        else:
+            self.mshfile = open(mshfile, 'r')
+
+    def parse(self):
+        if self.mshfile is None:
+            print("Meshfile not defined, can't parse!")
+            return
+
+        for line in self.mshfile.readlines():
+            tokens = line.split()
+            if len(tokens) != 7:
+                print("Illegal number of entries: " + str(len(tokens)))
+                continue
+
+            #x, y, z, vol, flux, dose, h20 = [float(p) for p in tokens]
+            self.data.append([float(p) for p in tokens])
+
+    def flattenflux_z(self, zmin, zmax):
+        pts = []
+        for d in self.data:
+            if zmin <= d[2] <= zmax:
+                pts.append([d[0], d[1], d[4]])
+        return pts
+
+    def reduce_range(self, pts, x, y, z):
+        return [[p[0], p[1], p[2]] for p in pts if
+                (x != None and x[0] <= p[0] <= x[1]) and
+                (y != None and y[0] <= p[1] <= y[1]) and
+                (z != None and z[0] <= p[2] <= z[1])]
+
+    def xxgrid(self, x, y, z, resX=100, resY=100):
+        "Convert 3 column data to matplotlib grid"
+        xi = numpy.linspace(min(x), max(x), resX)
+        yi = numpy.linspace(min(y), max(y), resY)
+        Z = matplotlib.mlab.griddata(x, y, z, xi, yi)
+        X, Y = numpy.meshgrid(xi, yi)
+        return X, Y, Z
+
+    def plotflux_z(self, zmin, zmax):
+        pts = self.flattenflux_z(zmin, zmax)
+        pts = self.reduce_range(pts, x=(-20, 20), y=(-20, 20), z=(0, 1e11))
+        xs = numpy.array([p[0] for p in pts])
+        ys = numpy.array([p[1] for p in pts])
+        fluxs = numpy.array([p[2] for p in pts])
+
+        fig = pyplot.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # 3-D Surface plot
+        triangles = matplotlib.tri.Triangulation(xs, ys)
+        surf = ax.plot_trisurf(xs, ys, fluxs, triangles, cmap=pyplot.get_cmap("jet"), lw=0.2,
+                alpha=0.5)
+
+        # 3-D Countour plot
+        X, Y, Z = self.xxgrid(xs, ys, fluxs)
+        #surf = ax.contour(X, Y, Z, cmap=pyplot.get_cmap("jet"), lw=0.2,
+        #        alpha=0.5)
+
+        #X, Y, Z = self.xxgrid(xs, ys, fluxs)
+        #surf = pyplot.contourf(X, Y, Z, 100, cmap="jet")
+
+        fig.colorbar(surf)
+        pyplot.show()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: python fluxplotter mshfile")
+    elif len(sys.argv) == 2:
+        p = FluxPlotter(sys.argv[1])
+        p.parse()
+        p.plotflux_z(-.5, .5)
+    else:
+        print("Too many args!")
+        print("Usage: python fluxplotter mshfile")
+
+    fig = pyplot.figure()
+    x = [0,1,2,3,4,5,6]
+    y3 = [5,6,8,3,4,6,1]
+    plot = pyplot.plot(x, y3)
+    pyplot.ylabel('Velocity [m/s]')
+    pyplot.xlabel('Temperature [eV]')
+    pyplot.legend(('T = 3 eV', 'T = 10 eV'))
+    pyplot.title("X-Velocity vs Temperature")
+    pyplot.show()
